eb2582c4ef3cd889b5c23c88076b6d40e99ba70a
japrine <jprine01@gmail.com>
2019-01-20 23:44:07 -0600

    quicksort

--- algorithms.py
+++ algorithms.py
@@ -15,23 +15,28 @@ def random_lister(x, z):  # x is length and y is int max
 
 def load_file(filename):
     data = []
-    with open('quicksort.txt') as f:
+    with open(filename) as f:
         for line in f:
             data.append(int(line))
     return data
 
 
 if __name__ == '__main__':
-    # Sort Merge
+    # For Sorts
     l = random_lister(100000, 100000)
+    # l = [3, 2, 5, 6, 1, 9, 7, 8, 0, 4, 10, 15, 14, 12, 11, 16, 17, 19, 18]
     print(l)
-    print(sortmerge.sort(l))
+
+    # Sort Merge
+    # print(sortmerge.sort(l))
 
     # Quck Sort
-    l = random_lister(100000, 100000)
-    print(l)
-    print(quicksort.first(l))
+    # print(quicksort.first(l))
+    # print(quicksort.last(l))
+    # print(quicksort.random(l))
+    print(quicksort.three(l))
+
 
     # Karatsuba Multiplication
-    print(karatsuba.multiply(3141592653589793238462643383279502884197169399375105820974944592,
-                             2718281828459045235360287471352662497757247093699959574966967627))
+    # print(karatsuba.multiply(3141592653589793238462643383279502884197169399375105820974944592,
+    #                          2718281828459045235360287471352662497757247093699959574966967627))
--- quicksort.py
+++ quicksort.py
@@ -1,30 +1,27 @@
 from random import randint
-from numpy import argsort
 
 
 def first(a, l=0, r=None):
-    if r is None:
-        r = len(a)
+    r = len(a) if r is None else r
     if r - l < 2:                          # Check Base Case
         return
     p, i = l, l+1                          # Assign Pivot
     for j in range(i, r):
-        if a[l] <= a[j]:                   # if j is larger
-            pass
+        if a[l] <= a[j]: pass              # if j is larger
         else:                              # if j is smaller
             a[i], a[j] = a[j], a[i]
             i += 1
     a[l], a[i-1] = a[i-1], a[l]
     first(a, l, i - 1)
     first(a, i, r)
+    return a
 
 
 def last(a, l=0, r=None):
-    if r is None:
-        r = len(a)
+    r = len(a) if r is None else r
     if r - l < 2:                          # Check Base Case
         return
-    p, i = r-1, l+1                          # Assign Pivot
+    p, i = r-1, l+1                        # Assign Pivot
     a[l], a[p] = a[p], a[l]
     for j in range(i, r):
         if a[l] <= a[j]:                   # if j is larger
@@ -35,11 +32,11 @@ def last(a, l=0, r=None):
     a[l], a[i-1] = a[i-1], a[l]
     last(a, l, i - 1)
     last(a, i, r)
+    return a
 
 
 def random(a, l=0, r=None):
-    if r is None:
-        r = len(a)
+    r = len(a) if r is None else r
     if r - l < 2:                          # Check Base Case
         return
     p, i = randint(l, r-1), l+1            # Assign Pivot
@@ -53,18 +50,31 @@ def random(a, l=0, r=None):
     a[l], a[i-1] = a[i-1], a[l]
     random(a, l, i - 1)
     random(a, i, r)
+    return a
 
 
 def three(a, l=0, r=None):
-    if r is None:
-        r = len(a)
+    r = len(a) if r is None else r
     if r - l < 2:                          # Check Base Case
         return
     if (r-l) % 2 == 0:
         _ = l + ((r-l)//2)-1
     else:
         _ = l + ((r-l)//2)
-    p = argsort([a[l], a[_], a[r-1]])[1]   # Assign Pivot
+    if a[l] <= a[_]:                        # Assign Pivot
+        if a[_] <= a[r - 1]:
+            p = _
+        elif a[l] <= a[r - 1]:
+            p = r - 1
+        else:
+            p = l
+    else:
+        if a[r - 1] <= a[_]:
+            p = _
+        elif a[r - 1] <= a[l]:
+            p = r - 1
+        else:
+            p = l
     if p == 0:
         p = l
     elif p == 1:
@@ -82,3 +92,4 @@ def three(a, l=0, r=None):
     a[l], a[i-1] = a[i-1], a[l]
     three(a, l, i - 1)
     three(a, i, r)
+    return a
