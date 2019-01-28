aabc229c8aec9ca84a40ceb382fc038fba4000a2
japrine <jprine01@gmail.com>
2019-01-28 03:05:11 -0600

    permutations

--- strings&arrays.py
+++ strings&arrays.py
@@ -38,26 +38,25 @@ def palindrome_permutations(s):
     output = []
 
     def add_letter(idx):
-        output.append([])
-        if len(output) == 1:
-            output[idx].append(s[idx])
-            return
-        for x in range(len(output[idx-1])):
+        new_output = []
+        if len(output) == 0:
+            new_output.append(s[idx])
+            return new_output
+        for x in range(len(output)):
             for _ in range(idx + 1):
-                string1 = output[idx-1][x][:_]
-                string2 = output[idx-1][x][_:]
+                string1 = output[x][:_]
+                string2 = output[x][_:]
                 string3 = s[idx]
-                output[idx].append(string1 + string3 + string2)
-        return
+                new_output.append(string1 + string3 + string2)
+        return new_output
 
     for _ in range(len(s)):
-        add_letter(_)
-    print(len(output[len(output)-1]))
-    return output[len(output)-1]
+        output = add_letter(_)
+    return len(output), output
 
 
-# print('Is Unique:', is_unique('Jon'))
-# print('Is Unique (no additional data structure):', is_unique_no_data('Jonath'))
-# print('Is String a permutation:', check_permutation('Jon', 'Jonathan'))
-# print(urlify('John Thomas Smith'))
-print('Palindromes :', palindrome_permutations('Adam'))
+print('Is Unique:', is_unique('Jon'))
+print('Is Unique (no additional data structure):', is_unique_no_data('Jonath'))
+print('Is String a permutation:', check_permutation('Jon', 'Jonathan'))
+print(urlify('John Thomas Smith'))
+print('Palindromes :', palindrome_permutations('John'))
